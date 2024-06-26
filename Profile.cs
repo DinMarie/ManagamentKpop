bea8a818eef9976db13d246abe3e9a5bdac36248
naval <naval@DESKTOP-UE2RTA0>
2024-06-26 23:48:10 +0800

    Add project files.

---
+++ Profile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelsa;
+using UI;
+
+
+
+namespace UI
+{
+    public class Profile
+    {
+        public void DisplayGroup(Group group)
+        {
+            Console.WriteLine("Group Name: " + group.Name);
+            Console.WriteLine("Group Members: " + group.Members);
+            Console.WriteLine("Fandom Name: " + group.FandomName);
+            Console.WriteLine("Debut Date: " + group.DebutDate);
+            Console.WriteLine("Company: " + group.Company);
+        }
+    }
+
+}
